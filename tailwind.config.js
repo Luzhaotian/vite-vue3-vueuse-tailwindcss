1422dd174b99d05e840e5e7b88f0734f0e7754b4
卢照天 <2430485982@qq.com>
2024-03-29 22:29:25 +0800

    去除无用代码

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	// content: ["./src/**/*.{html,js}"],
-  content: [
-    './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}'
-  ],
-  darkMode: 'media',
+	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+	darkMode: "media",
 	theme: {
 		extend: {},
 	},
